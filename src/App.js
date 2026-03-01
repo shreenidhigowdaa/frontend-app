c0d7d993ec79cb56871388595224cb155f92020d
shreenidhigowdaa <shreenidhis809@gmail.com>
2026-03-01 22:23:12 +0530

    Fetch and display data from API in App component
    Fetch data from an API and display it in the component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://YOUR-EC2-IP:5000/data")
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Database Time</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
